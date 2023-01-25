8d79fc00b066c5a95afb8bceb84b8c4fad39f383
mydreamis-18 <hitoryok@naver.com>
2023-01-25 15:33:49 +0900

    feat 확장자 추가 및 테스트 파일 생성

--- back/app.js
+++ back/app.js
@@ -9,18 +9,22 @@ let globSource;
 
 // 비동기 방식인 ES 모듈을 커먼 JS로 가져오기
 const ipfsClientFn = async () => {
-    //
-    const { create, globSource } = await import("ipfs-http-client");
-    return { create, globSource };
+  //
+  const { create, globSource } = await import("ipfs-http-client");
+  return { create, globSource };
 };
 
 (async () => {
-    //
-    const ipfsClent = await ipfsClientFn();
-    ipfs = ipfsClent.create({ host: 'localhost', port: '5002', protocol: 'http' });
-    globSource = ipfsClent.globSource;
-    ipfs.addAll
-    ipfs.add
+  //
+  const ipfsClent = await ipfsClientFn();
+
+  ipfs = ipfsClent.create({
+    host: "localhost",
+    port: "5002",
+    protocol: "http",
+  });
+
+  globSource = ipfsClent.globSource;
 })();
 
 app.use(cors({ origin: "http://localhost:3000" }));
@@ -28,46 +32,54 @@ app.use(cors({ origin: "http://localhost:3000" }));
 app.listen(PORT, () => console.log("back server start..."));
 
 app.post("/", multer().array("files"), async (req, res) => {
+  //
+  // console.log(ipfs === undefined)
+  // while (true) {
+  //     if (ipfs !== undefined) {
+  //         break;
+  //     }
+  // }
+
+  if (ipfs === undefined) {
+    res.send("ipfs 모듈 import 중이니 잠시 후 다시 시도해주세요.");
+    return;
+  }
+
+  console.log(req.files);
+  console.log(req.files.length);
+
+  const imgsData = new Array(0);
+
+  try {
     //
-    // console.log(ipfs === undefined)
-    // while (true) {
-    //     if (ipfs !== undefined) {
-    //         break;
-    //     }
-    // }
-
-    if (ipfs === undefined) {
-        res.send("ipfs 모듈 import 중이니 잠시 후 다시 시도해주세요.");
-        return;
-    }
-
-    console.log(req.files);
-    console.log(req.files.length);
-
-    const imgsData = new Array(0);
-    
-    try {
+    await Promise.all(
+      req.files.map(async (file) => {
         //
-        await Promise.all(req.files.map(async (file) => {
-            //
-            imgsData.push(await ipfs.add({ path: file.originalname, content: file.buffer }));
-        }));
+        imgsData.push(
+          await ipfs.add({
+            path: decodeURIComponent(file.originalname),
+            content: file.buffer,
+          })
+        );
+      })
+    );
+  } catch (error) {
+    //
+    res.send("ipfs daemon이 실행 중인지 확인해주세요.");
+    return;
+  }
 
-    } catch (error) {
-        res.send("ipfs daemon이 실행 중인지 확인해주세요.");
-        return;
-    }
-    
-    // 해당 경로의 모든 jpg 파일 저장
-    // const imgPaths = new Array(0);
-    // for await (const iterator of ipfs.addAll(globSource("./", "**/*.png"))) {
-    //     imgPaths.push("http://localhost:9090/ipfs/" + iterator.cid);
-    // }
-    // console.log(imgPaths);
+  // 해당 경로의 모든 jpg 파일 저장
+  // const imgPaths = new Array(0);
+  // for await (const iterator of ipfs.addAll(globSource("./", "**/*.jpg"))) {
+  //     imgPaths.push("http://localhost:9090/ipfs/" + iterator.cid);
+  // }
+  // console.log(imgPaths);
 
-    // const string = await ipfs.add("string");
-    // const stringPath = "https://ipfs.io/ipfs/" + string.cid;
-    // console.log(stringPath);
+  // const string = await ipfs.add("string");
+  // const stringPath = "https://ipfs.io/ipfs/" + string.cid;
+  // console.log(stringPath);
 
-    res.send(imgsData);
-})
+  console.log(imgsData);
+  res.send(imgsData);
+});
--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 import axios from "axios";
 import { useState, useRef } from "react";
 
-const ipfsFn = async (files, setImgs, setJsonData) => {
+const ipfsFn = async (files, setImgs, setDocs) => {
   //
   if (files.length === 0) {
     alert("파일 선택이 되지 않았습니다.");
@@ -15,12 +15,19 @@ const ipfsFn = async (files, setImgs, setJsonData) => {
   Array.from({ length: files.length }, (v, i) => i).forEach((i) => {
     //
     // 파일명이 한글일 경우에 깨지는 현상을 방지하고자 인코딩 하여 전송
-    const { type, size, lastModified, lastModifiedDate, webkitRelativePath } = files[0];
-    const options = { type, size, lastModified, lastModifiedDate, webkitRelativePath };
+    const { type, size, lastModified, lastModifiedDate, webkitRelativePath } = files[i];
+
+    const options = {
+      type,
+      size,
+      lastModified,
+      lastModifiedDate,
+      webkitRelativePath,
+    };
     const fileName = encodeURIComponent(files[i].name);
 
     const file = new File([files[i]], fileName, options);
-    formData.append(`files`, file);
+    formData.append("files", file);
     fileTypes.push(type);
   });
 
@@ -30,59 +37,87 @@ const ipfsFn = async (files, setImgs, setJsonData) => {
 
   const filesData = (await axios.post("http://localhost:8282", formData)).data;
 
-  if ((typeof filesData) === "string") {
+  if (typeof filesData === "string") {
     //
     alert(filesData);
     return;
   }
 
-  // 온전한 파일명 확인 가능
-  console.log(filesData.map((data) => decodeURIComponent(data.path)));
-
+  // 온전한 파일명인지 확인
+  console.log(filesData.map((data) => data.path));
   console.log(fileTypes);
+
+  const imgs = new Array(0);
+  const docs = new Array(0);
 
   filesData.forEach((file, idx) => {
     //
-    const imgs = new Array(0);
-    const jsonData = new Array(0);
+    // 다양한 파일 타입별 처리 필요 => fileTypes[idx]
+    // 요구사항 => jpg, jpeg, gif, png, bmp, doc, docx, xlsx, xls, pdf, hwp 첨부 가능
+    const imageTypes = ["image/jpeg", "image/gif", "image/png", "image/bmp"];
+    const documentTypes = [
+      "application/msword",
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      "application/vnd.ms-excel",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "application/pdf",
+      "application/haansofthwp",
+    ];
 
-    // 다양한 파일 타입별 처리 필요
-    switch (fileTypes[idx]) {
-      //
-      case "image/jpeg":
-        imgs.push("http://localhost:9090/ipfs/" + file.cid["/"]);
-        break;
+    const isImage = imageTypes.some((type) => type === fileTypes[idx]);
+    const isDocument = documentTypes.some((type) => type === fileTypes[idx]);
 
-      case "application/haansoftxlsx":
-        jsonData.push("http://localhost:9090/ipfs/" + file.cid["/"]);
-        break;
+    const path = "http://localhost:9090/ipfs/" + file.cid["/"];
 
-      default:
-        break;
+    const fileOriginalName = filesData[idx].path;
+    const extentionName = fileOriginalName.split(".")[fileOriginalName.split(".").length - 1];
+
+    if (isImage) {
+      imgs.push({ path, extentionName });
     }
 
-    setImgs(imgs);
-    setJsonData(jsonData);
-    console.log(imgs, jsonData);
-  })
-}
+    if (isDocument) {
+      docs.push({ path, extentionName });
+    }
+  });
+
+  setImgs(imgs);
+  setDocs(docs);
+};
 
 function App() {
   //
   const file = useRef();
   const [imgs, setImgs] = useState();
-  const [jsonData, setJsonData] = useState();
+  const [docs, setDocs] = useState();
   //
   return (
     <div className="App">
       <input type="file" ref={file} multiple />
-      <button onClick={() => ipfsFn(file.current.files, setImgs, setJsonData)}>ipfs</button>
-      {/* ------------------------- */}
-      {/* 이미지 파일일 경우 보여주기 */}
+      <button onClick={() => ipfsFn(file.current.files, setImgs, setDocs)}>ipfs</button>
+      {/* ---------- */}
+      {/* 이미지 파일 */}
       {imgs && imgs.map((path, index) => <img src={path} key={index} alt="" />)}
-      {/* -------------------------------- */}
-      {/* 엑셀 파일일 경우 다운로드 버튼 생성 */}
-      {jsonData && jsonData.map((path, index) => <button onClick={() => window.open(path)}>{"excel 파일" + (index + 1) + " 다운로드"}</button>)}
+      {imgs &&
+        imgs.map((obj, index) => (
+          <button onClick={() => window.open(obj.path)} key={index}>
+            {`.${obj.extentionName} 파일 다운로드`}
+          </button>
+        ))}
+      {/* -------- */}
+      {/* 문서 파일 */}
+      {docs &&
+        docs.map((obj, index) => (
+          <button onClick={() => window.open(obj.path)} key={index}>
+            {`.${obj.extentionName} 파일 다운로드`}
+          </button>
+        ))}
+      {/* {docs &&
+        docs.map((path, index) => (
+          <a href={path} key={index}>
+            {"excel 파일" + (index + 1) + " 다운로드"}
+          </a>
+        ))} */}
     </div>
   );
 }
